b3bda108785a0ab727e33cab458ab8080d6b3685
Xiang-M-J <70873544+Xiang-M-J@users.noreply.github.com>
2025-06-11 17:32:49 +0800

    update

---
+++ lib/line_chart.dart
@@ -0,0 +1,203 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+
+class EmotionLineChart extends StatefulWidget {
+  const EmotionLineChart({super.key, required this.spots});
+  final List<FlSpot> spots;
+  @override
+  State<EmotionLineChart> createState() => _EmotionLineChartState();
+}
+
+class _EmotionLineChartState extends State<EmotionLineChart> {
+  List<int> showingTooltipOnSpots = [1, 3, 5];
+
+  Widget bottomTitleWidgets(double value, TitleMeta meta, double chartWidth) {
+    final style = TextStyle(
+      fontWeight: FontWeight.bold,
+      fontSize: 18 * chartWidth / 500,
+    );
+
+    int timestamp = value.toInt();
+    DateTime dateTime = DateTime.fromMillisecondsSinceEpoch(timestamp);
+    String text =
+        "${dateTime.minute.toString().padLeft(2, '0')}:${dateTime.second.toString().padLeft(2, '0')}:${(dateTime.millisecond / 10).ceil().toString().padLeft(2, '0')}";
+
+    return SideTitleWidget(
+      axisSide: meta.axisSide,
+      child: Text(text, style: style),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (widget.spots.isEmpty) {
+      return LineChart(LineChartData(
+        titlesData: const FlTitlesData(show: false),
+        gridData: const FlGridData(show: false),
+        borderData: FlBorderData(
+          show: true,
+          border: Border.all(
+            width: 1.5,
+            color: Colors.blueGrey,
+          ),
+        ),
+        backgroundColor: Colors.grey,
+      ));
+    }
+
+    final lineBarsData = [
+      LineChartBarData(
+        showingIndicators: showingTooltipOnSpots,
+        spots: widget.spots,
+        isCurved: true,
+        barWidth: 4,
+        shadow: const Shadow(
+          blurRadius: 8,
+        ),
+        belowBarData: BarAreaData(
+          show: true,
+        ),
+        dotData: const FlDotData(show: false),
+      ),
+    ];
+
+    final tooltipsOnBar = lineBarsData[0];
+
+    return AspectRatio(
+      aspectRatio: 2.5,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 24.0,
+          vertical: 10,
+        ),
+        child: LayoutBuilder(builder: (context, constraints) {
+          return LineChart(
+            LineChartData(
+              showingTooltipIndicators: showingTooltipOnSpots.map((index) {
+                return ShowingTooltipIndicators([
+                  LineBarSpot(
+                    tooltipsOnBar,
+                    lineBarsData.indexOf(tooltipsOnBar),
+                    tooltipsOnBar.spots[index],
+                  ),
+                ]);
+              }).toList(),
+              lineTouchData: LineTouchData(
+                enabled: true,
+                handleBuiltInTouches: false,
+                touchCallback:
+                    (FlTouchEvent event, LineTouchResponse? response) {
+                  if (response == null || response.lineBarSpots == null) {
+                    return;
+                  }
+                  if (event is FlTapUpEvent) {
+                    final spotIndex = response.lineBarSpots!.first.spotIndex;
+                    setState(() {
+                      if (showingTooltipOnSpots.contains(spotIndex)) {
+                        showingTooltipOnSpots.remove(spotIndex);
+                      } else {
+                        showingTooltipOnSpots.add(spotIndex);
+                      }
+                    });
+                  }
+                },
+                mouseCursorResolver:
+                    (FlTouchEvent event, LineTouchResponse? response) {
+                  if (response == null || response.lineBarSpots == null) {
+                    return SystemMouseCursors.basic;
+                  }
+                  return SystemMouseCursors.click;
+                },
+                getTouchedSpotIndicator:
+                    (LineChartBarData barData, List<int> spotIndexes) {
+                  return spotIndexes.map((index) {
+                    return TouchedSpotIndicatorData(
+                      const FlLine(
+                        color: Colors.pink,
+                      ),
+                      FlDotData(
+                        show: true,
+                        getDotPainter: (spot, percent, barData, index) =>
+                            FlDotCirclePainter(
+                          radius: 8,
+                          strokeWidth: 2,
+                        ),
+                      ),
+                    );
+                  }).toList();
+                },
+                touchTooltipData: LineTouchTooltipData(
+                  tooltipBgColor: Colors.pink,
+                  tooltipRoundedRadius: 8,
+                  getTooltipItems: (List<LineBarSpot> lineBarsSpot) {
+                    return lineBarsSpot.map((lineBarSpot) {
+                      return LineTooltipItem(
+                        lineBarSpot.y.toString(),
+                        const TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      );
+                    }).toList();
+                  },
+                ),
+              ),
+              lineBarsData: lineBarsData,
+              minY: 0,
+              titlesData: FlTitlesData(
+                leftTitles: const AxisTitles(
+                  axisNameWidget: Text('count'),
+                  axisNameSize: 24,
+                  sideTitles: SideTitles(
+                    showTitles: false,
+                    reservedSize: 0,
+                  ),
+                ),
+                bottomTitles: AxisTitles(
+                  sideTitles: SideTitles(
+                    showTitles: true,
+                    interval: 1,
+                    getTitlesWidget: (value, meta) {
+                      return bottomTitleWidgets(
+                        value,
+                        meta,
+                        constraints.maxWidth,
+                      );
+                    },
+                    reservedSize: 30,
+                  ),
+                ),
+                rightTitles: const AxisTitles(
+                  axisNameWidget: Text('count'),
+                  sideTitles: SideTitles(
+                    showTitles: false,
+                    reservedSize: 0,
+                  ),
+                ),
+                topTitles: const AxisTitles(
+                  axisNameWidget: Text(
+                    'Wall clock',
+                    textAlign: TextAlign.left,
+                  ),
+                  axisNameSize: 24,
+                  sideTitles: SideTitles(
+                    showTitles: true,
+                    reservedSize: 0,
+                  ),
+                ),
+              ),
+              gridData: const FlGridData(show: false),
+              borderData: FlBorderData(
+                show: true,
+                border: Border.all(
+                  width: 1.5,
+                  color: Colors.blueGrey,
+                ),
+              ),
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'dart:async';
 import 'dart:io';
 import 'dart:math';
-
 import 'package:fl_chart/fl_chart.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:verapp/utils.dart';
 import 'package:verapp/utils/emotion_labels.dart';
 import 'package:verapp/utils/emotion_recognition.dart';
-import 'package:verapp/utils/image_converter.dart';
+import 'line_chart.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -72,7 +73,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Timer? _timer;
   Timer? _cTimer;
   Duration _recordingDuration = Duration.zero;
-  final Duration _maxRecordingDuration = const Duration(seconds: 1 * 10);
+  final Duration _maxRecordingDuration = const Duration(seconds: 1 * 60);
 
   double _time = 0;
 
@@ -82,7 +83,14 @@ class _MyHomePageState extends State<MyHomePage> {
 
   bool isControllerInitialized = false;
 
+  final List<List<String>> _storeData = List.empty(growable: true);
+
   final List<CameraImage> _frameBuffer = [];
+
+  String nowEmotion = "";
+  int count = 0;
+
+  Stopwatch stopwatch = Stopwatch();
 
   void _onNewFrame(CameraImage image) async {
     // 限制最多保留 30 帧
@@ -92,15 +100,42 @@ class _MyHomePageState extends State<MyHomePage> {
     // List<Uint8List> imageData = getDatafromCameraImage(image);
     _frameBuffer.add(image);
     // print(_frameBuffer.length);
+    // just_once(image);
+  }
+
+  void predictOneImage(CameraImage image)async{
     int? result = await emotionRecognizer?.predictAsync(image);
-    if (result != null) print(emoLabels[result]);
+    if (result != null) {
+      _storeData.add([getNowTime(), emoLabels[result]]);
+      print(emoLabels[result]);
+      setState(() {
+        nowEmotion = emoLabels[result];
+        _time += 0.5;
+        _points.add(FlSpot(_time, result.toDouble()));
+
+        if (_points.length > 30) {
+          _points.removeAt(0);
+        }
+      });
+    }
+  }
+
+  void just_once(CameraImage image){
+    if (count >= 1) {
+      return;
+    }
+    count += 1;
+    stopwatch.start();
+    predictOneImage(image);
+    stopwatch.stop();
+    print(stopwatch.elapsedMilliseconds / 1000);
   }
 
   @override
   void initState() {
     super.initState();
     _initializeCamera();
-    _startSineWave();
+    // _startSineWave();
     emotionRecognizer = EmotionRecognizer();
     _initModel();
   }
@@ -111,34 +146,59 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _initializeCamera() async {
     _cameras = await availableCameras();
-    _controller = CameraController(_cameras[0], ResolutionPreset.medium);
+
+    if (_cameras.isEmpty) {
+      showToast("未找到摄像头");
+      return;
+    }
+    CameraDescription? bestCamera;
+    for (var i = 0; i < _cameras.length; i++) {
+      if (_cameras[i].lensDirection == CameraLensDirection.front) {
+        bestCamera = _cameras[i];
+        break;
+      } else if (_cameras[i].lensDirection == CameraLensDirection.external) {
+        bestCamera = _cameras[i];
+      }
+    }
+    if (bestCamera != null) {
+      _controller = CameraController(bestCamera, ResolutionPreset.medium);
+    }else{
+      _controller = CameraController(_cameras[0], ResolutionPreset.medium);
+    }
+    
     // await _controller!.initialize();
     if (mounted) setState(() {});
   }
 
-  void _startSineWave() {
-    _timer = Timer.periodic(const Duration(milliseconds: 100), (_) {
-      setState(() {
-        _time += 0.1;
-        _points.add(FlSpot(_time, sin(_time)));
+  // void _startSineWave() {
+  //   _timer = Timer.periodic(const Duration(milliseconds: 100), (_) {
+  //     setState(() {
+  //       _time += 0.1;
+  //       _points.add(FlSpot(_time, sin(_time)));
 
-        // 保持最多100个点
-        if (_points.length > 100) {
-          _points.removeAt(0);
-        }
-      });
-    });
-  }
+  //       // 保持最多100个点
+  //       if (_points.length > 100) {
+  //         _points.removeAt(0);
+  //       }
+  //     });
+  //   });
+  // }
 
   void _stopRecording() async {
     if (_controller != null && _isRecording) {
       await _controller!.stopImageStream();
+      await _controller!.pausePreview();
       setState(() {
         _isRecording = false;
       });
       _cTimer?.cancel();
       _cTimer = null;
     }
+  }
+
+  String getNowTime() {
+    DateTime dateTime = DateTime.now();
+    return dateTime.toString();
   }
 
   void _startRecording() async {
@@ -184,11 +244,45 @@ class _MyHomePageState extends State<MyHomePage> {
     super.dispose();
   }
 
-  Widget _buildCameraPreview() {
-    if (_controller == null || !_controller!.value.isInitialized) {
-      return const Center(child: CircularProgressIndicator());
+  Future<void> exportFile() async {
+    const headers = "时间,情感\n";
+    String content = "";
+
+    for (var i = 0; i < _storeData.length; i++) {
+      content += "${_storeData[i][0]}, ${_storeData[i][1]}\n";
     }
-    if (!_isRecording) {
+    final dir = await _getDownloadDirectory();
+    String filepath = '${dir.path}/${getNowTime()}.csv';
+    final file = File(filepath);
+
+    file.writeAsString(headers + content);
+
+    showToast("文件已保存在$filepath");
+  }
+
+  Future<Directory> _getDownloadDirectory() async {
+    if (Platform.isAndroid) {
+      // Android: 请求存储权限
+      if (!await _requestStoragePermission()) {
+        throw Exception('Storage permission denied');
+      }
+      return Directory('/storage/emulated/0/Download');
+    } else {
+      // iOS: 使用文档目录
+      return getApplicationDocumentsDirectory();
+    }
+  }
+
+  Future<bool> _requestStoragePermission() async {
+    final status = await Permission.storage.request();
+    return status.isGranted;
+  }
+
+  Widget _buildCameraPreview() {
+    // if (_controller == null || !_controller!.value.isInitialized) {
+    //   return const Center(child: CircularProgressIndicator());
+    // }
+    if (!_isRecording || _controller == null || !_controller!.value.isInitialized) {
       return Container(
         color: Colors.grey[300],
         child: const Center(
@@ -202,27 +296,27 @@ class _MyHomePageState extends State<MyHomePage> {
     return CameraPreview(_controller!);
   }
 
-  Widget _buildChart() {
-    return LineChart(
-      LineChartData(
-        minX: _points.isNotEmpty ? _points.first.x : 0,
-        maxX: _points.isNotEmpty ? _points.last.x : 10,
-        minY: -1,
-        maxY: 1,
-        lineBarsData: [
-          LineChartBarData(
-            spots: _points,
-            isCurved: true,
-            color: Colors.green,
-            barWidth: 2,
-          ),
-        ],
-        titlesData: const FlTitlesData(show: false),
-        gridData: const FlGridData(show: false),
-        borderData: FlBorderData(show: false),
-      ),
-    );
-  }
+  // Widget _buildChart() {
+  //   return LineChart(
+  //     LineChartData(
+  //       minX: _points.isNotEmpty ? _points.first.x : 0,
+  //       maxX: _points.isNotEmpty ? _points.last.x : 10,
+  //       minY: -1,
+  //       maxY: 1,
+  //       lineBarsData: [
+  //         LineChartBarData(
+  //           spots: _points,
+  //           isCurved: true,
+  //           color: Colors.green,
+  //           barWidth: 2,
+  //         ),
+  //       ],
+  //       titlesData: const FlTitlesData(show: true),
+  //       gridData: const FlGridData(show: false),
+  //       borderData: FlBorderData(show: false),
+  //     ),
+  //   );
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -237,16 +331,16 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
               // 摄像头区域（40%）
               SizedBox(
-                height: height * 0.58,
+                height: height * 0.48,
                 child: _buildCameraPreview(),
               ),
 
               // 图表区域（30%）
               SizedBox(
-                height: height * 0.2,
+                height: height * 0.3,
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: _buildChart(),
+                  child: EmotionLineChart(spots: _points),
                 ),
               ),
 
@@ -266,7 +360,7 @@ class _MyHomePageState extends State<MyHomePage> {
                         child: const Text('停止录制'),
                       ),
                       ElevatedButton(
-                        onPressed: _startRecording,
+                        onPressed: exportFile,
                         child: const Text('导出文件'),
                       ),
                     ],
